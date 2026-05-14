705584e8caf328740d0890b5b2258dfd918c0611
SACHITVERMA <vermasachit658@gmail.com>
2026-05-14 22:49:29 +0530

    checking the math methods

---
+++ math.js
@@ -0,0 +1,14 @@
+let a = Math.PI;
+console.log(a);
+
+console.log(Math.fround(a)); //for round off the value
+console.log(Math.trunc(a)); //to truncate the value
+// let b = 125.65;
+// console.log(Math.floor(b)); //it is working for the negative value bestly
+
+// console.log(Math.ceil(b)); //it is workking for the positive value bestly
+
+let b = -125.65;
+console.log(Math.floor(b)); //it is working for the negative value bestly
+
+console.log(Math.ceil(b)); //it is workking for the positive value bestly
