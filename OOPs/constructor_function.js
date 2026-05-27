7217c3aa408b0372699978cda31f2b2a86f7e705
SACHITVERMA <vermasachit658@gmail.com>
2026-05-27 23:21:33 +0530

    making some changes

--- OOPs/constructor_function.js
+++ OOPs/constructor_function.js
@@ -8,7 +8,7 @@ function person(name, age, gender) {
   this.name = name;
   this.age = age;
   this.gender = gender;
-  return this; // Return the current object instance
+  // Return the object created by the constructor function
   //   console.log(this);
   //
 }
@@ -19,3 +19,18 @@ const person2 = new person("Alice", 25, "female");
 
 console.log(person1);
 console.log(person2);
+console.log(person2.name); // Accessing properties of the object
+
+//factory function is a function that returns an object. It is a simple way to create objects without using classes or constructor functions. A factory function can take parameters that are used to initialize the object's properties.
+
+// Example of a factory function in JavaScript:
+//factory function are that type of function in which it doesn't directly sent the object .In which we are creating a object and then return that object to the user. It is a simple way to create objects without using classes or constructor functions. A factory function can take parameters that are used to initialize the object's properties.
+function createPerson(name, age) {
+  return {
+    name: name,
+    age: age,
+  };
+}
+
+const person3 = createPerson("Bob", 30);
+console.log(person3);
